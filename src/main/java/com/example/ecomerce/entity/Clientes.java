8cd5a9170b5242d78f9bfeda7b3c5a9bedcd17a5
Alexandre Miranda <alexpmiranda26@gmail.com>
2025-01-23 10:02:28 -0300

    Criando Entity clientes

---
+++ src/main/java/com/example/ecomerce/entity/Clientes.java
@@ -0,0 +1,43 @@
+package com.example.ecomerce.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Clientes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Clientes {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String senha;
+
+    private String cpf;
+
+    private String endereco;
+
+    private String data_nascimento;
+
+    @Column(name = "`timestamp`")
+    @CreationTimestamp
+    private Date created_at;
+
+}
